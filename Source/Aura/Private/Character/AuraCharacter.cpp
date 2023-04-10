c5ed355163bffde259e0495b160c867da5ea2b6a
Dan Stull <danstull12_99@yahoo.com>
2023-04-10 16:12:01 -0700

    Main and Enemy Classes

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright Dan Stull 
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+// Copyright Dan Stull 
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
